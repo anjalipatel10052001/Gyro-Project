83673fe9bebbad3061c5973700d4ec96656c80fc
anjalipatel10052001 <153019810+anjalipatel10052001@users.noreply.github.com>
2023-12-06 10:55:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
-app.get('public/gyro.html', (req, res)=> {
-  res.sendFile(path.join(__dirname, 'public', 'gyro.html'));
-});
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}/gyro.html`);
 });
